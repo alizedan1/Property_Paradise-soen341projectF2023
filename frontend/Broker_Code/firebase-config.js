f963c85d277d566acabb45b6bba4656cf86b7150
TheMasterProgrammer <ryanrebbas2001@gmail.com>
2023-10-27 16:43:31 -0400

    changes

--- frontend/Broker_Code/firebase-config.js
+++ frontend/Broker_Code/firebase-config.js
@@ -1,11 +1,12 @@
 // Initialize Firebase
 var firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+    apiKey: "AIzaSyD2s0r0eAXlBUQ22UEpEpSF6d03FgFsBb0",
+    authDomain: "propertyparadise-83a03.firebaseapp.com",
+    projectId: "propertyparadise-83a03",
+    storageBucket: "propertyparadise-83a03.appspot.com",
+    messagingSenderId: "452198388057",
+    appId: "1:452198388057:web:d4029f9a5678364f8b7c48",
+    measurementId: "G-C7G8NXT1PD"
 };
 
 firebase.initializeApp(firebaseConfig);
